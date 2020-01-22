3dd22a88180d8d256c8b19258bcc646909be5dc7
Pedro Sequeira <pedro.sequeira@itsector.pt>
2020-01-22 10:29:44 +0000

    [Fix] - Simple search for username
    [Description]
    There were files missing in this main branch for it to work correctly.

--- app/src/main/java/com/example/android/codewars/models/User.kt
+++ app/src/main/java/com/example/android/codewars/models/User.kt
@@ -1,13 +1,6 @@
 package com.example.android.codewars.models
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class User(
     val username: String,
-    val name: String,
-    val honor: Int,
-    val clan: String,
-    val leaderboardPosition: Int
-) : Parcelable
+    val name: String
+)
